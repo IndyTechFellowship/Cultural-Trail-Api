a8213cb46612ed220df27293a1cd2ea6c3825e34
root <root@ip-172-31-49-229.ec2.internal>
2016-08-08 22:44:45 +0000

    update prod item and write some scripts to start and kill server

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cultural_trail_api, CulturalTrailApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "ec2-52-206-122-212.compute-1.amazonaws.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
